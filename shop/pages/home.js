72b3eb89c5a6101dc400c5199247867c6d82af1e
ColinLiu <colinlets@gmail.com>
2019-01-06 16:38:42 +0800

    首页填充数据

--- shop/pages/home.js
+++ shop/pages/home.js
@@ -1,85 +1,20 @@
 // pages/home.js
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-      slideshow: [{
-          'id': 1,
-          'img': 'http://192.168.0.105/wechat/slideshow_01.png',
-      },{
-          'id': 2,
-          'img': 'http://192.168.0.105/wechat/slideshow_02.png',
-      },{
-          'id': 3,
-          'img': 'http://192.168.0.105/wechat/slideshow_03.png',
-      },{
-          'id': 4,
-          'img': 'http://192.168.0.105/wechat/slideshow_04.png',
-      },],
+      slideshow: [],
       indicatorDots: true,
       autoplay: true,
       interval: 5000,
       duration: 1000,
       imageHeight: 0,
-      goodsList: [{
-          'id': 1,
-          'name': '小迷糊玻尿酸补水黑膜',
-          'desc': '让肌肤回归净润清透！',
-          'price': 69,
-          'origin_price': 299,
-          'img': 'http://192.168.0.105/wechat/goods_001.jpg',
-      },{
-          'id': 2,
-          'name': '小迷糊玻尿酸补水黑膜',
-          'desc': '让肌肤回归净润清透！',
-          'price': 69,
-          'origin_price': 299,
-          'img': 'http://192.168.0.105/wechat/goods_002.jpg',
-      },{
-          'id': 3,
-          'name': '小迷糊玻尿酸补水黑膜',
-          'desc': '让肌肤回归净润清透！',
-          'price': 69,
-          'origin_price': 299,
-          'img': 'http://192.168.0.105/wechat/goods_003.jpg',
-      },{
-          'id': 4,
-          'name': '小迷糊玻尿酸补水黑膜',
-          'desc': '让肌肤回归净润清透！',
-          'price': 69,
-          'origin_price': 299,
-          'img': 'http://192.168.0.105/wechat/goods_004.jpg',
-      },{
-          'id': 5,
-          'name': '小迷糊玻尿酸补水黑膜',
-          'desc': '让肌肤回归净润清透！',
-          'price': 69,
-          'origin_price': 299,
-          'img': 'http://192.168.0.105/wechat/goods_005.jpg',
-      },{
-          'id': 6,
-          'name': '小迷糊玻尿酸补水黑膜',
-          'desc': '让肌肤回归净润清透！',
-          'price': 69,
-          'origin_price': 299,
-          'img': 'http://192.168.0.105/wechat/goods_006.jpg',
-      },{
-          'id': 7,
-          'name': '小迷糊玻尿酸补水黑膜',
-          'desc': '让肌肤回归净润清透！',
-          'price': 69,
-          'origin_price': 299,
-          'img': 'http://192.168.0.105/wechat/goods_007.jpg',
-      },{
-          'id': 8,
-          'name': '小迷糊玻尿酸补水黑膜',
-          'desc': '让肌肤回归净润清透！',
-          'price': 69,
-          'origin_price': 299,
-          'img': 'http://192.168.0.105/wechat/goods_008.jpg',
-      },],
+      goodsList: [],
+      page: 1,
   },
   imageLoad: function(e){
     let windowWidth = wx.getSystemInfoSync().windowWidth;
@@ -94,12 +29,42 @@ Page({
           url: '/pages/single?id='+e.currentTarget.dataset.id,
       })
   },
+  getSlideshow: function(){
+    let _this = this;
+    wx.request({
+        url: app.globalData.baseApi + "home/slideshow",
+        method: "GET",
+        success(res){
+            if (res.data.code == 200){
+                _this.setData({
+                    slideshow: res.data.list
+                });
+            }
+        }
+    });
+  },
+  getList: function(){
+    let _this = this;
+    wx.request({
+        url: app.globalData.baseApi + "home/list",
+        method: "GET",
+        data: {page: _this.data.page},
+        success(res){
+            if (res.data.code == 200){
+                _this.setData({
+                    goodsList: res.data.data.list
+                })
+            }
+        }
+    });
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getSlideshow();
+    this.getList();
   },
 
   /**
