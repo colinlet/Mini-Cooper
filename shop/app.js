85d65a458452d06d8d4135c5c0b2b95ce0e151ce
ColinLiu <colinlets@gmail.com>
2019-01-13 16:42:06 +0800

    移除敏感信息

--- shop/app.js
+++ shop/app.js
@@ -1,9 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    //获取用户授权
-    this.checkAuth();
-    //初始化购物车信息
+    //初始化信息
     try{
       const jsonStr = wx.getStorageSync('cart-list');
       if (!jsonStr) return true;
@@ -25,47 +23,6 @@ App({
     session: "",
     userInfo: {},
     contactInfo: "",
-  },
-  checkAuth: function(){
-    try{
-      const session = wx.getStorageSync("session");
-      if (session) {
-        this.globalData.session = session;
-        return true;
-      }
-    }catch(e){
-      console.log(e.toString());
-      return false;
-    }
-    let _this = this;
-    wx.login({
-        success(res){
-          if (res.code){
-            _this.login(res.code);
-          } else {
-            console.log("登录失败！"+res.errMsg);
-          }
-        }
-    });
-  },
-  login: function(code) {
-    let _this = this;
-    wx.request({
-        url: _this.globalData.baseApi + "user/login",
-        method: "GET",
-        data: {code: code},
-        success(res){
-          if (res.data.code === 200) {
-            _this.globalData.session = res.data.session;
-            try{
-              wx.setStorageSync("session", res.data.session);
-            }catch(e){
-              console.log(e.toString());
-              return false;
-            }
-          }
-        }
-    })
   },
   showContact: function() {
     if (this.globalData.contactInfo.length < 1){
